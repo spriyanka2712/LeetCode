ab7fe21db1084602942ed6c42c2d2c2d89d53221
Priyanka S <spriyanka27122005@gmail.com>
2026-09-10 20:09:13 +0530

    Time: 5 ms (82.52%), Space: 44.2 MB (99.97%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int rev = 0;
+       int temp = x;
+       while(x > 0){
+        int rem = x % 10;
+        rev = (rev*10) + rem;
+        x /= 10;
+       }
+       return rev == temp;
+    }
+}
