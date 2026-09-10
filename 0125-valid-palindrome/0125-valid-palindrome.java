c4ec507ad80cf9b2a245235ee7b7290aee0abcb9
Priyanka S <spriyanka27122005@gmail.com>
2026-09-10 20:15:35 +0530

    Time: 4 ms (44.37%), Space: 45 MB (37.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        String original = sb.toString();
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+}
