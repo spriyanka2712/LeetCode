d9e67f22ba072db3c7eda0fab95057bb3ab6371f
Priyanka S <spriyanka27122005@gmail.com>
2026-09-14 15:15:02 +0530

    Time: 0 ms (100%), Space: 43.6 MB (13.17%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = n-1;
+        while(i<=j){
+            if( s.charAt(i) == s.charAt(j)){
+                return i;
+            }
+            i++;
+            j--;
+        }
+        return -1;
+    }
+}
