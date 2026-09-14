819d4ea6d945a0863055596cd3cf273eead2f124
Priyanka S <spriyanka27122005@gmail.com>
2026-09-14 15:12:32 +0530

    Time: 1 ms (100%), Space: 46.1 MB (99.33%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        int res[] = new int[friends.length];
+        int index = 0;
+        for(int i = 0; i < order.length; i++){
+            for(int j = 0; j < friends.length; j++){
+                if(order[i] == friends[j]){
+                    res[index] = order[i];
+                    index += 1;
+                }
+            }
+        }
+        return res;
+    }
+}
