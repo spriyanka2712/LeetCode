b20bdec55c7ed88a235836ba32a67ea24ac49c7a
Priyanka S <spriyanka27122005@gmail.com>
2026-09-14 15:02:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (44.31%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int temp = x;
+        int sum = 0;
+        while(temp > 0){
+            sum += (temp%10);
+            temp /= 10;
+        }
+        if(x%sum == 0){
+            return sum;
+        }
+        return -1;
+    }
+}
